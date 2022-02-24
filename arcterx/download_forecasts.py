6d365ef8b90ab1eea5947d1a2c8e433b42ef3f76
Andre Amador <a1amador@cordc-andre.ucsd.edu>
2022-02-24 12:30:40 -0800

    minor fixex

--- arcterx/download_forecasts.py
+++ arcterx/download_forecasts.py
@@ -11,7 +11,6 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 from wgpack.config import seachest_data_dir
 datadir  = os.path.join(os.path.dirname(seachest_data_dir),'ARCTERX2022/forecasts')
-# datadir  = '/data/seachest/cordc-data/PROJECTS/ARCTERX2022/forecasts'
 
 ta = datetime.datetime.utcnow().strftime('%Y-%m-%dT')+'00%3A00%3A00Z&'
 tb = (datetime.datetime.utcnow() + pd.Timedelta(days=5)).strftime('%Y-%m-%dT')+'00%3A00%3A00Z&'
@@ -20,6 +19,13 @@ tb = (datetime.datetime.utcnow() + pd.Timedelta(days=5)).strftime('%Y-%m-%dT')+'
 # ----------------------------------------------------------------------------------------------------------------------
 # LOAD ROMS data
 # https://www.pacioos.hawaii.edu/currents/model-mariana/
+# PacIOOS’ Regional Ocean Modeling System (ROMS) provides a 6-day, 3-hourly forecast for the region surrounding the
+# Mariana Islands, including Guam and the main islands of the Commonwealth of the Northern Mariana Islands (CNMI),
+# at approximately 2-km (1.2-mile) resolution. The forecast is run daily and gets updated on this website around
+# 9:30 AM Chamorro Standard Time (UTC+10) every morning. ROMS (http://myroms.org) is an open source,
+# community-supported model widely adopted by the scientific community for a diverse range of applications. Model runs
+# are configured and produced for the Mariana region by Dr. Brian Powell and lab within the Department of Oceanography
+# in the School of Ocean and Earth Science and Technology (SOEST) at the University of Hawaiʻi at Mānoa.
 # ----------------------------------------------------------------------------------------------------------------------
 
 # url
@@ -61,6 +67,15 @@ if dwld_flg:
 # ----------------------------------------------------------------------------------------------------------------------
 # Load WRF data
 # https://www.pacioos.hawaii.edu/weather/model-wind-mariana/
+# PacIOOS’ Weather Research and Forecasting (WRF) model provides a 6-day, hourly forecast for the region surrounding
+# the Mariana Islands, including Guam and the main islands of the Commonwealth of the Northern Mariana Islands (CNMI),
+# at approximately 3-km (1.9-mile) resolution. The forecast is run daily and gets updated on this website between
+# 9:30 AM and 10:30 AM Chamorro Standard Time (UTC+10) every morning. WRF is an open source numerical weather
+# prediction system widely adopted by the scientific community for atmospheric research and operational forecasting.
+# Shown here is the Advanced Research WRF (ARW) dynamical solver developed and maintained by the Mesoscale and
+# Microscale Meteorology (MMM) Laboratory of the National Center for Atmospheric Research (NCAR). Model runs are
+# configured and produced for the Mariana region by Dr. Yi-Leng Chen and lab within the Department of Atmospheric
+# Sciences in the School of Ocean and Earth Science and Technology (SOEST) at the University of Hawaiʻi at Mānoa.
 # ----------------------------------------------------------------------------------------------------------------------
 
 # url
@@ -102,6 +117,11 @@ if dwld_flg:
 # ----------------------------------------------------------------------------------------------------------------------
 # Load WW3 data
 # https://www.pacioos.hawaii.edu/waves/model-mariana/
+# PacIOOS’ WaveWatch III (WW3) model is a 5-day, hourly forecast for the region surrounding the Mariana Islands,
+# including Guam and the main islands of the Commonwealth of the Northern Mariana Islands (CNMI), at approximately
+# 0.05-degree (~5-km) resolution. The forecast is run daily and gets updated on this website around 10:00 PM Chamorro
+# Standard Time (UTC+10) each day. This regional wave model helps capture island effects such as island shadowing,
+# refraction, and accurate modeling of local wind waves. WW3 (https://polar.ncep.noaa.gov/waves/wavewatch/) is an open source wave model developed by the National Oceanic and Atmospheric Administration (NOAA) National Centers for Environmental Prediction (NCEP). Model runs are configured and produced for the Mariana region by Dr. Kwok Fai Cheung and lab within the Department of Ocean and Resources Engineering (ORE) in the School of Ocean and Earth Science and Technology (SOEST) at the University of Hawaiʻi at Mānoa. Boundary conditions are provided by the global WaveWatch III model (ww3_global) at approximately 50-km resolution.
 # ----------------------------------------------------------------------------------------------------------------------
 
 # url
