196cbc6943d165bae230b23d5f511d57f9714214
Andre Amador <a1amador@cordc-andre.ucsd.edu>
2022-06-21 14:12:25 -0700

    removing unnecessary files

--- xmls/new_WPT.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'LOGIN TO WGMS'
-/usr/bin/curl -k -H "Content-Type: text/xml; charset=utf-8" --dump-header headers -H "SOAPAction:" -d @login.xml -X POST https://gliders.wgms.com/webservices/entityapi.asmx
-
-echo ''
-echo 'ISSUING A NEW WAYPOINT'
-/usr/bin/curl -X POST  -L -b headers -d @wgms_new_WPT.xml http://gliders.wgms.com/webservices/entityapi.asmx --header "Content-Type:text/xml"
-echo ''
